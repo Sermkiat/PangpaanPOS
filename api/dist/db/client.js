d76a7e3ddb11303c1d516908e2a107377745b2d7
Sermkiat <sermkiat.sr@gmail.com>
2025-11-23 16:01:03 +0700

    chore: drop api build artifacts

--- api/dist/db/client.js
+++
@@ -1,9 +0,0 @@
-import "dotenv/config";
-import { drizzle } from "drizzle-orm/node-postgres";
-import pkg from "pg";
-import * as schema from "./schema.js";
-const { Pool } = pkg;
-const connectionString = process.env.DATABASE_URL ||
-    "postgresql://pang:pangpass@localhost:5432/pangpaan_pos";
-export const pool = new Pool({ connectionString });
-export const db = drizzle(pool, { schema });
